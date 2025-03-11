ff24136952cabb25ecb4ee5656628b22f3cabd17
Ashish Lukka <ashishlukka2005@gmail.com>
2025-03-11 19:42:44 +0530

    server.js changes

--- server/server.js
+++ server/server.js
@@ -1,37 +1,33 @@
 const exp = require("express");
 const app = exp();
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const userApp = require("./APIs/userApi");
 const authorApp = require("./APIs/authorApi");
 const adminApp = require("./APIs/adminApi");
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
-
-const port = process.env.PORT || 4000;
-
-// Establish database connection
-console.log("DBURL from env:", process.env.DBURL);
-mongoose.connect(process.env.DBURL)
-    .then(() => {
-        app.listen(port, () => console.log(`Server is running on port ${port}...`));
-        console.log("Database connection successful");
-    })
-    .catch(err => console.log("Database connection failed: ", err));
-
-// Middleware for parsing JSON request bodies
 app.use(exp.json());
 
-// Register API routes
-app.use('/user-api', userApp);  // Routes for user-related operations
-app.use("/author-api", authorApp);  // Routes for author-related operations
-app.use('/admin-api', adminApp);  // Routes for admin-related operations
+// Database connection
+console.log("DBURL from env:", process.env.DBURL);
+mongoose
+  .connect(process.env.DBURL)
+  .then(() => console.log("Database connection successful"))
+  .catch((err) => console.log("Database connection failed: ", err));
+
+
+// API routes
+app.use("/api/user", userApp);
+app.use("/api/author", authorApp);
+app.use("/api/admin", adminApp);
 
 // Global error handling middleware
 app.use((err, req, res, next) => {
-    console.log("Error encountered in Express error handler:", err);
-    res.status(500).send({ message: err.message });
+  console.log("Error encountered:", err);
+  res.status(500).send({ message: err.message });
 });
 
+// Export the app for Vercel
 module.exports = app;
