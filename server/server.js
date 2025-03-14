c786c92fce9ea1195d489da2722bf4d157a2205e
Ashish Lukka <ashishlukka2005@gmail.com>
2025-03-14 22:29:57 +0530

    browser icon changed

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
-
+  
 // Global error handling middleware
 app.use((err, req, res, next) => {
   console.log("Error encountered:", err);
